179c6377357434aa8d95cbb51859aab58b6a2bf3
Robin Marchart <robin.marchart@tum.de>
2022-08-18 13:41:14 +0200

    more unstable shit

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 #![no_std]
 
-use core::{sync::atomic::AtomicBool, ffi::{c_int, c_void}, mem::transmute, slice::from_raw_parts_mut, panic::PanicInfo};
+use core::{sync::atomic::AtomicBool, mem::transmute, slice::from_raw_parts_mut, panic::PanicInfo};
 
 use rand_chacha::ChaCha20Rng;
 
 use rand::{Rng,SeedableRng};
 
+
 struct InitData{
-    malloc:fn(usize)->*mut c_void,
+    malloc:fn(usize)->*mut (),
     rand:ChaCha20Rng
 }
 
@@ -29,9 +30,9 @@ fn get_init_data()->InitData{
               }
           }
           None=>{
-              let malloc:fn(usize)->*mut c_void=transmute(dlsym(RTDL_NEXT as *mut c_void, b"malloc\0".as_ptr()));
-              let read:fn(c_int,*mut u8,usize)->isize=transmute(dlsym(RTDL_DEFAULT as *mut c_void, b"read\0".as_ptr()));
-              let open:fn(*const u8,c_int)->c_int=transmute(dlsym(RTDL_DEFAULT as *mut c_void, b"open\0".as_ptr()));
+              let malloc:fn(usize)->*mut ()=transmute(dlsym(RTDL_NEXT as *mut (), b"malloc\0".as_ptr()));
+              let read:fn(i32,*mut u8,usize)->isize=transmute(dlsym(RTDL_DEFAULT as *mut (), b"read\0".as_ptr()));
+              let open:fn(*const u8,i32)->i32=transmute(dlsym(RTDL_DEFAULT as *mut (), b"open\0".as_ptr()));
               let mut seed:[u8;32]=[0;32];
               read(open(b"/dev/urandom\0".as_ptr(),0),seed.as_mut_ptr(),seed.len());
               let mut rand=ChaCha20Rng::from_seed(seed);
@@ -53,7 +54,7 @@ fn get_init_data()->InitData{
 
 
 #[no_mangle]
-pub extern "C" fn malloc(size:usize)->*mut c_void{
+pub extern "C" fn malloc(size:usize)->*mut (){
     let mut init_data=get_init_data();
     unsafe{
         let ptr=(init_data.malloc)(size);
@@ -68,12 +69,12 @@ pub extern "C" fn malloc(size:usize)->*mut c_void{
 
 #[link(name="dl")]
 extern {
-    fn dlsym(handle:*mut c_void,symbol:*const u8)->*const c_void;
+    fn dlsym(handle:*mut (),symbol:*const u8)->*const ();
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     unsafe {
-       transmute::<*const c_void,fn ()->!>(dlsym(RTDL_DEFAULT as *mut c_void, b"abort\0".as_ptr()))();
+       transmute::<*const (),fn ()->!>(dlsym(RTDL_DEFAULT as *mut (), b"abort\0".as_ptr()))();
     }
 }
